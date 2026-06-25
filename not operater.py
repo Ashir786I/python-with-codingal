173b3d8ed0335100d680587ab83d1c5aec7f07ad
Ashir786I <ashiriqbal27102023@gmail.com>
2026-06-25 18:52:08 +0200

    Add files via upload

---
+++ not operater.py
@@ -0,0 +1,19 @@
+a=10
+b=12
+c=12
+
+print(not (a==b))
+
+print(not (a==c))
+
+
+a="coding"
+b="python"
+
+if not a==b:
+    print(a, "is not the same as", b,)
+
+a=int(input("Enter a number"))
+
+if not (a % 2 == 0):
+    print(a, "is an odd number.")
