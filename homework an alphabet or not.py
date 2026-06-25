9be9fab2da0b5e93fd09dfa47ae68b7322360e9b
Ashir786I <ashiriqbal27102023@gmail.com>
2026-06-25 19:05:37 +0200

    Add files via upload

---
+++ homework an alphabet or not.py
@@ -0,0 +1,7 @@
+a=input("Enter a a letter or number")
+letter= a or b or c or d or e or f or g or h or i or j or k or l or m or n or o or p or q or r or s or t or u or u or v or w or x or y or z
+
+if (a == letter):
+        print(a, "is a letter")
+else:
+    print (a, "is not a letter")
