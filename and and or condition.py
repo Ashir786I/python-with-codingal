91d409a7152cae982fc4348019e1736533fd2c48
Ashir786I <ashiriqbal27102023@gmail.com>
2026-06-25 18:51:40 +0200

    Add files via upload

---
+++ and and or condition.py
@@ -0,0 +1,23 @@
+a=29
+b=-34
+c=0
+
+if a>0 or b>0 or c>0:
+        print("Either one of the numbers is greater then 0")
+else:
+        print("None of the Numbers is greather then 0")
+
+
+if a>0 and b>0 and c>0:
+    print("All of thr numbers are greater then 0")
+else:
+    print ("Not all of the numbers are greater then 0")
+
+a=27
+b=87
+c=0
+
+if a and b and c :
+      print("All three numbers have value as true")
+else:
+    print("Either one value has at least as true")
