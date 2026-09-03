37eccb25dd9d379d228c1f92a01edd68f0a2593b
Ashir786I <ashiriqbal27102023@gmail.com>
2026-09-03 18:52:19 +0200

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,19 @@
+a=9
+b=11
+c=2
+d=4
+z=0
+z=(a+b)*c/d
+
+print(z)
+
+name="John"
+age=12
+
+if  name == "John" or "Alex" and age >= 2:
+    print("Hello")
+
+else :
+     print("Good Bye")      
+
+
